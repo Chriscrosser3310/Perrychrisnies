266830a5e38e57bd7a0b56edbb0f143e5ff066ff
Jielun (Chris) Chen <31495624+Chriscrosser3310@users.noreply.github.com>
2023-01-18 16:16:35 -0800

    Update age.js

--- js/age.js
+++ js/age.js
@@ -1,6 +1,6 @@
 let ageEl = document.getElementById("age");
 
 setInterval(() => {
-	let time = (new Date() - new Date(1547877065000)) / (1000 * 60 * 60 * 24 * 365.25); // milliseconds per year
+	let time = (new Date() - new Date(1547800000000)) / (1000 * 60 * 60 * 24 * 365.25); // milliseconds per year
 	ageEl.innerText = time.toString().substring(0, 12);
 }, 50);
